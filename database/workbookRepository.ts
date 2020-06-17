8776e760eedd9f644dd4731bede1b1787fe3aeb8
Motti Bechhofer <MBechhofer@chorus.cloud>
2020-06-17 16:06:41 +0300

    remove logs

--- database/workbookRepository.ts
+++ database/workbookRepository.ts
@@ -10,15 +10,12 @@ export class workbookRepository extends memoryRepository<workbook> {
     }
     getAll() {
         const workbooks = this.items;
-        console.log("workbooks", workbooks);
         return Promise.resolve(workbooks);
     }
     findByString(query: string) {
         const words = query.split(' ');
-        console.log("words", words);
         const result = this.items.filter(item => {
             for (let [key, value] of Object.entries(item)) {
-                console.log("value", value);
                 if (words.indexOf(value) != -1) {
                     return true;
                 }
