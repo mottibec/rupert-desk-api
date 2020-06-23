57d6d8d432b2fd927ec7205e2701e7df3ee77b28
Motti Bechhofer <MBechhofer@chorus.cloud>
2020-06-23 12:35:54 +0300

    init
    use tsvector and tsquery over name and id
    remove all src/ files

--- src/models/user.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var user = /** @class */ (function () {
-    function user() {
-    }
-    return user;
-}());
-exports.user = user;
